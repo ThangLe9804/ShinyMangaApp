c1507c9a33b046364cbf3df335cc1cda850ac344
Thang Le <thangle9804@gmail.com>
2025-04-22 00:40:54 +0700

    Initial Commit

---
+++ ShinyMangaApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ShinyMangaApp
+//
+//  Created by Thang Le on 22/4/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ShinyMangaAppTests/ShinyMangaAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShinyMangaAppTests.swift
+//  ShinyMangaAppTests
+//
+//  Created by Thang Le on 22/4/25.
+//
+
+import Testing
+@testable import ShinyMangaApp
+
+struct ShinyMangaAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
